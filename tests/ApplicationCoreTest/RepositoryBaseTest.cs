acf391fe1646a9c1767c5648f8e74cd62508e197
李凤焕 <huanent@hotmail.com>
2017-09-21 09:01:13 +0800

    添加Repository测试

---
+++ tests/ApplicationCoreTest/RepositoryBaseTest.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationCore.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationCoreTest
+{
+    [TestClass]
+    public class RepositoryBaseTest
+    {
+        [TestMethod]
+        public void FindTest()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase("TestDb");
+            var context = new AppDbContext(optionsBuilder.Options);
+            var demoSet = context.Set<Demo>();
+            var id = new Guid("dddddddd-dddd-dddd-dddd-dddddddddddd");
+
+            demoSet.Add(new Demo
+            {
+                Id = id,
+                Name = "a"
+            });
+            context.SaveChanges();
+
+            var context2 = new AppDbContext(optionsBuilder.Options);
+            var demoSet2 = context.Set<Demo>();
+            var entity = demoSet2.Find(id);
+
+            Assert.IsNotNull(entity);
+            Assert.AreEqual(entity.Id, id);
+            Assert.AreEqual(entity.Name, "a");
+        }
+
+    }
+}
