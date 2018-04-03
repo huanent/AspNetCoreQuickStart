05495fad1298091f690bb50c8122c6c20f979243
李凤焕 <huanent@hotmail.com>
2018-04-03 23:46:45 +0800

    新增缓存key

---
+++ src/ApplicationCore/CacheKeys.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore
+{
+    public sealed class CacheKeys
+    {
+        public const string DEMO_KEY = nameof(DEMO_KEY);
+    }
+}
